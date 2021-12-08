a6ecde2314072d3a23ecd683813891301e94946a
hkapoor2001 <hkapoor2001@gmail.com>
2021-12-08 08:39:25 -0500

    Final Commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,80 +7,106 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(GSDevTools, MotionPathPlugin, MotionPathHelper, MorphSVGPlugin, DrawSVGPlugin);
 
-gsap.set("#wing-fade-in", { alpha:0 });
-gsap.set("#body-fade-in", { alpha:0 });
-gsap.set("#waterdrop-1", { alpha:0 });
 
-
-MorphSVGPlugin.convertToPath("#circle-1") 
+GSDevTools.create();
 
 const mainTL = gsap.timeline();
 
+
+MorphSVGPlugin.convertToPath("#circle-drop") 
+MorphSVGPlugin.convertToPath("#tail-circle-1")
+MorphSVGPlugin.convertToPath("#tail-circle-2")
+MorphSVGPlugin.convertToPath("#tail-circle-3")
+MorphSVGPlugin.convertToPath("#circledrop-1")
+MorphSVGPlugin.convertToPath("#circledrop-2")
+MorphSVGPlugin.convertToPath("#circledrop-3")
+MorphSVGPlugin.convertToPath("#circledrop-2b")
+
+
+
+
+
+gsap.set("#nesting",{alpha:0})
+gsap.set("#house",{alpha:0})
+
+
+gsap.set("#waterdrop",{alpha:0})
+gsap.set("#tail-waterdrop-1",{alpha:0})
+gsap.set("#tail-waterdrop-2",{alpha:0})
+gsap.set("#tail-waterdrop-3",{alpha:0})
+gsap.set("#waterdrop-3",{alpha:0})
+gsap.set("#waterdrop-2",{alpha:0})
+gsap.set("#waterdrop-2b",{alpha:0})
+gsap.set("#waterdrop-1",{alpha:0})
+
+
+
 function waterdrop(){
     const tl = gsap.timeline();
-    tl.to("#circle-1", {duration:.3, motionPath:{path:"#waterdrop-motion-path", align:"self"}, morphSVG:"#waterdrop-1", transformOrigin: "80% 50%", alpha:0})
-    return tl
+    tl.to("#circle-drop", {motionPath:{path:"#waterdrop-motion-path", align:"self"}, morphSVG:"#waterdrop", transformOrigin: "80% 50%", duration:.3})
+    tl.to("#circle-drop", {alpha:0, duration:.1})
+    return tl;
 }
+
 
 function tailsplash(){
     const tl = gsap.timeline();
-    tl.from(".drawsvgtailsplash",{drawSVG: "100%", duration:.1, stagger:.08})
-    tl.to(".drawsvgtailsplash",{drawSVG: "0", duration:.1, stagger:.08})
-    return tl
+    tl.to("#tail-circle-1", {alpha:1, motionPath:{path:"#tail-motion-path-1", align:"self"}, morphSVG:"#tail-waterdrop-1", transformOrigin: "80% 50%", duration:.1},"same1")
+    tl.to("#tail-circle-1", {alpha:0, duration:.2})
+    tl.to("#tail-circle-2", {alpha:1, motionPath:{path:"#tail-motion-path-2", align:"self"}, morphSVG:"#tail-waterdrop-2", transformOrigin: "80% 50%", duration:.15},"same1")
+    tl.to("#tail-circle-2", {alpha:0, duration:.3} )
+    tl.to("#tail-circle-3", {alpha:1, motionPath:{path:"#tail-motion-path-3", align:"self"}, morphSVG:"#tail-waterdrop-3", transformOrigin: "80% 50%", duration:.18},"same1")
+    tl.to("#tail-circle-3", {alpha:0, duration:.1})
+    return tl;
 }
 
-function tailsplashfade(){
+function bodysplash(){
     const tl = gsap.timeline();
-    tl.to(".tailsplash",{alpha:0, duration:.06, stagger:.08})
-    return tl
+    tl.to("#circledrop-3", {alpha:1, motionPath:{path:"#waterdrop-motionpath-3", align:"self"}, morphSVG:"#waterdrop-3", transformOrigin: "80% 50%", duration:.1, delay:.38},"same1")
+    tl.to("#circledrop-3", {alpha:0, duration:.2})
+    tl.to("#circledrop-2", {alpha:1, motionPath:{path:"#waterdrop-motionpath-2", align:"self"}, morphSVG:"#waterdrop-2", transformOrigin: "80% 50%", duration:.1, delay:.68},"same1")
+    tl.to("#circledrop-2", {alpha:0, duration:.2})
+    tl.to("#circledrop-2b", {alpha:1, motionPath:{path:"#waterdrop-motionpath-2b", align:"self"}, morphSVG:"#waterdrop-2b", transformOrigin: "80% 50%", duration:.1, delay:.75},"same1")
+    tl.to("#circledrop-2b", {alpha:0, duration:.2})
+    tl.to("#circledrop-1", {alpha:1, motionPath:{path:"#waterdrop-motionpath-1", align:"self"}, morphSVG:"#waterdrop-1", transformOrigin: "80% 50%", duration:.1, delay:.8},"same1")
+    tl.to("#circledrop-1", {alpha:0, duration:.2})
+    return tl;
 }
+
+
+
 
 function motionpathwings(){
     const tl = gsap.timeline();
-    tl.to("#wing-wave-1", {duration: 3.5, motionPath:{path:"#wing-motion-path-1", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#wing-wave-2", {duration: 3.5, motionPath:{path:"#wing-motion-path-2", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#wing-wave-3", {duration: 3.5, motionPath:{path:"#wing-motion-path-3", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#wing-wave-4", {duration: 3.5, motionPath:{path:"#wing-motion-path-4", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    return tl
-}
-
-function motionpathbody(){
-    const tl = gsap.timeline();
-    tl.to("#body-wave-1", {duration: 3.5, motionPath:{path:"#body-motion-path-1", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#body-wave-2", {duration: 3.5, motionPath:{path:"#body-motion-path-2", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#body-wave-3", {duration: 3.5, motionPath:{path:"#body-motion-path-3", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
-    tl.to("#body-wave-4", {duration: 3.5, motionPath:{path:"#body-motion-path-4", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#wing-wave-1", {duration: 3, motionPath:{path:"#wing-motion-path-1", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#wing-wave-2", {duration: 3, motionPath:{path:"#wing-motion-path-2", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#wing-wave-3", {duration: 3, motionPath:{path:"#wing-motion-path-3", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#wing-wave-4", {duration: 3, motionPath:{path:"#wing-motion-path-4", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
     return tl;
 }
 
-function bodysplashes(){
+function body(){
     const tl = gsap.timeline();
-    tl.from("#drawsvg-waterdrop-2",{drawSVG: "100%", duration:.9, delay:.01}, "samesplash")
-    tl.to("#drawsvg-waterdrop-2",{drawSVG: "0%"}, "samesplash")
-    tl.from("#drawsvg-waterdrop-3",{drawSVG: "100%", duration:.9, delay:.005}, "samesplash")
-    tl.to("#drawsvg-waterdrop-3",{drawSVG: "0"}, "samesplash")
-}
-
-function bodyfadein(){
-    const tl = gsap.timeline();
-    tl.from("#body-fade-in", {alpha:0})
-    tl.to("#body-fade-in", {alpha:1})
+    tl.to("#body-wave-1", {duration: 3, motionPath:{path:"#body-motion-path-1", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#body-wave-3", {duration: 3, motionPath:{path:"#body-motion-path-3", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
+    tl.to("#body-wave-4", {duration: 3, motionPath:{path:"#body-motion-path-4", align:"self"},rotation: 35, scale:2.5, transformOrigin: "50% 50%"}, "same")
     return tl;
 }
 
-function wingfadein(){
+
+
+
+function text1(){
     const tl = gsap.timeline();
-    tl.from("#wing-fade-in", {alpha:0})
-    tl.to("#wing-fade-in", {alpha:1} )
+    tl.from(".circles", {alpha:0, duration:.01})
+    tl.to(".circles", {motionPath:{path:"#circlesmotionpath", align:"self"}, alpha:1, duration:2})
     return tl;
 }
 
-mainTL.add(waterdrop());
-mainTL.add(tailsplash(), "sametl");
-mainTL.add(tailsplashfade());
-mainTL.add(motionpathwings(), "sametl");
-mainTL.add(motionpathbody(), "sametl");
-mainTL.add(bodysplashes(), "sametl");
-mainTL.add(bodyfadein(), "sametl2");
-mainTL.add(wingfadein(), "sametl2");
-GSDevTools.create();
+
+mainTL.add(waterdrop())
+      .add(tailsplash(),"same1")
+      .add(bodysplash(),"same1")
+      .add(motionpathwings(),"same1")
+      .add(body(),"same1")
+      .add(text1(),"same1")
